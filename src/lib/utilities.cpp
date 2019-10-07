14532c1c9e9462b67fa31c69a3260a283d60ba6d
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-10-07 18:09:29 +1300

    Add some handy utility functions.

--- src/lib/utilities.cpp
+++ src/lib/utilities.cpp
@@ -29,3 +29,49 @@ QMimeData *setMimeDataText(const QString& text, bool isExistingFileName)
 
     return mimeData;
 }
+
+QString strippedName(const QString &fullFileName)
+{
+    return QFileInfo(fullFileName).fileName();
+}
+
+QString fileContents(const QString &fullFileName)
+{
+    QString contents;
+    QFile file(fullFileName);
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+        QTextStream in(&file);
+        contents = in.readAll();
+    }
+    file.close();
+
+    return contents;
+}
+
+QString previousLocation(const QString &settingPath)
+{
+    QSettings settings;
+
+    settings.beginGroup(settingPath);
+    QString location = settings.value("location", QDir::homePath()).toString();
+    settings.endGroup();
+
+    return location;
+}
+
+void setPreviousLocation(const QString &settingPath, const QString &location)
+{
+    QSettings settings;
+
+    settings.beginGroup(settingPath);
+    settings.setValue("location", location);
+    settings.endGroup();
+}
+
+QString fileDirectory(const QString &fileName)
+{
+    QDir d = QFileInfo(fileName).absoluteDir();
+    QString absolutePath = d.absolutePath();
+
+    return absolutePath;
+}
--- src/lib/utilities.h
+++ src/lib/utilities.h
@@ -6,3 +6,19 @@
 void writeToFile(const QString &fileName, const QString &content);
 
 QMimeData *setMimeDataText(const QString& text, bool isExistingFileName = false);
+
+QString strippedName(const QString &fullFileName);
+
+QString fileContents(const QString &fullFileName);
+
+QString previousLocation(const QString &settingPath);
+
+void setPreviousLocation(const QString &settingPath, const QString &location);
+
+/**
+ * Return the absolute directory path to the given file name.
+ *
+ * @param fileName The file name to return the path to.
+ * @return The absoulte path to the directory part of the given file name.
+ */
+QString fileDirectory(const QString &fileName);
